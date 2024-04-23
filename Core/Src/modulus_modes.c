f7fd03540faf49ab4bc73f8647d4db48c151dbc7
Jakub Frąckowiak <264306@student.pwr.edu.pl>
2024-04-23 23:25:46 +0200

    Added FFT and RFFT functions from arm_math

--- Core/Src/modulus_modes.c
+++ Core/Src/modulus_modes.c
@@ -10,16 +10,26 @@
 
 #include <stdint.h>
 
+
 void dummy_mode(float* input,float* output)
 {
 	// fft
+
+	arm_rfft_fast_instance_f32 fft;
+
+	arm_rfft_fast_init_f32 (&fft, 4096);
+
+	float rfft[4096]={0};
+
+	arm_rfft_fast_f32(&fft, input, rfft, 0);
 	// vector
 
 	// do something
 
 	// rfft
+	arm_rfft_fast_f32(&fft, rfft, output, 1);
 
-	// outpyt
+	// output
 }
 
 modulus_mode modes_list[MODES_COUNT]={&dummy_mode};
